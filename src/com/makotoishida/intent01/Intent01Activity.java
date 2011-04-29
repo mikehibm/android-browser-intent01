da58f7425ea4426db9741ec3435a37515b631c79
Makoto Ishida <makotoishida@gmail.com>
2011-04-29 13:07:50 -1000

    good

--- src/com/makotoishida/intent01/Intent01Activity.java
+++ src/com/makotoishida/intent01/Intent01Activity.java
@@ -3,56 +3,73 @@ package com.makotoishida.intent01;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class Intent01Activity extends Activity {
 
+	static ArrayAdapter<String> adapter;
+	ListView list;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.main);
         
+        if (adapter == null){
+        	adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        }
+        
+        list = (ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        processIntent(getIntent());
     }
-	
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		
-		Log.d("test", "onDestroy");
-	}
 	
 	@Override
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
 
-        procData(intent);
+		processIntent(intent);
 	}
 	
-	private void procData(Intent intent) {
+	private void processIntent(Intent intent) {
     	
     	if (Intent.ACTION_VIEW.equals(intent.getAction()) ){
 			String url = intent.getDataString();
 			
-			//TextViewにURLを表示
-			TextView txt = (TextView)findViewById(R.id.txtUrl);
-			txt.setText(url);
+			//リスト表示用の配列に追加
+			if (!isExisting(url)){
+				adapter.insert(url, 0);				
+			}
 			
-			//外部ブラウザで開く
-			sendViewIntent(url);
+			//標準ブラウザで開く
+			Intent newintent = new Intent(); 
+			newintent.fillIn(intent, 0);
+			newintent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
+			startActivity(newintent);
     	}
-    }
 
-    private void sendViewIntent(String url){
-		Intent intent = (Intent)getIntent().clone();
-//		intent.setAction(Intent.ACTION_VIEW);
-//		intent.addCategory(Intent.CATEGORY_DEFAULT);
-//		intent.addCategory(Intent.CATEGORY_BROWSABLE);
-//		intent.setData(Uri.parse(url));
-		intent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
-		startActivity(intent);
-    }
+		//TextViewに件数を表示
+		TextView txt = (TextView)findViewById(R.id.txtCount);
+    	if (adapter.getCount() > 0){
+    		txt.setText("Count: " + adapter.getCount());
+    	} else {
+    		txt.setText(R.string.initial_msg);
+    	}
+	}
+	
+	private boolean isExisting(String url){
+		for (int i = 0; i < adapter.getCount(); i++) {
+			if (adapter.getItem(i).equals(url) ){
+				return true;
+			}
+		}
+		return false;
+	}
+
 
 
 }
