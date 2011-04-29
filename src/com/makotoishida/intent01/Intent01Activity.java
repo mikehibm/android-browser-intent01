5412b59cb822f3977af1066c410bec5eba98cd49
Makoto Ishida <makotoishida@gmail.com>
2011-04-28 21:40:50 -1000

    testing

--- src/com/makotoishida/intent01/Intent01Activity.java
+++ src/com/makotoishida/intent01/Intent01Activity.java
@@ -1,13 +1,45 @@
 package com.makotoishida.intent01;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Intent01Activity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+        procData();
     }
+	
+	private void procData() {
+    	Intent intent = getIntent();
+    	
+    	if (Intent.ACTION_VIEW.equals(intent.getAction()) ){
+			String url = intent.getDataString();
+			
+			//TextViewにURLを表示
+			TextView txt = (TextView)findViewById(R.id.txtUrl);
+			txt.setText(url);
+			
+			//外部ブラウザで開く
+			sendViewIntent(url);
+    	}
+    }
+
+    private void sendViewIntent(String url){
+		Intent intent = (Intent)getIntent().clone();
+//		intent.setAction(Intent.ACTION_VIEW);
+//		intent.addCategory(Intent.CATEGORY_DEFAULT);
+//		intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//		intent.setData(Uri.parse(url));
+		intent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
+		startActivity(intent);
+    }
+
+
 }
